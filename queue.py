736c918dca1e832c23355c9dabe8024d020d7e5c
Modi Himanshu <himanshum.wappnet@gmail.com>
2025-01-28 12:37:04 +0530

    updated queue  h

--- queue.py
+++ queue.py
@@ -33,4 +33,26 @@ q.dequeue()
 q.printvalue()
 
 
+class queue_himanshu:
+    def __init__(self):
+        self.value=[]
+    
+    def enqueue(self,value):
+        print("you push :",value)
+        self.value.append(value)
+        
+    def dequeue(self):
+        if len(self.value)==0:
+            print("queue is empty")
+        else:
+            front=self.value[0]
+            print("remove",front)
+            self.value=self.value[1:]
+            
+    def printvalue(self):
+        print("queue is : ")
+        for i in self.value:
+            print(i,end=" ")
+
+
  # type: ignore
