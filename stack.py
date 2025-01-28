c58f1b51e69c37b5cd82a169aa97eba4f519ce6c
Modi Himanshu <himanshum.wappnet@gmail.com>
2025-01-28 12:11:58 +0530

    chnage the stack file

--- stack.py
+++ stack.py
@@ -37,3 +37,29 @@ q.top()
 q.printvalue()
 
 
+class himu:
+    def __init__(self):
+        self.value=[]
+    
+    def push(self,value):
+        print("push",value)
+        self.value=[value]+self.value
+        
+    def pop(self):
+        if len(self.value)==0:
+            print("stack is empty")
+        else:
+            n=len(self.value)
+            first=self.value[n-1]
+            print("pop",first)
+            self.value=self.value[:-1]
+            
+        
+    def printvalue(self):
+        print("stack is : ")
+        for i in self.value:
+            print(i,end=" ")
+
+    def top(self):
+        first=self.value[0] 
+        print("top is ",first)
